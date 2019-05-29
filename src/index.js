4b03483f814c9a04a68f4da996b89a62c480f399
Aniyus Kujur <aniyus@MacBook-Pro-3.local>
2019-05-29 23:52:34 +0530

    1st version

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require('offline-plugin/runtime').install();
+
+import './fonts/libre-baskerville-v5-latin-regular.woff';
+import './fonts/libre-baskerville-v5-latin-regular.woff2';
+
+import './index.html';
+import './index.scss';
+import './scripts/script.js';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+const title = 'My Minimal React Webpack Babexxl Setup';
+
+ReactDOM.render(<div>sssss</div>,document.getElementById('app'));
+
---
+++ src/scripts/script.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const title = 'My Minimal React Webpack Babexxl Setup';
+
+ReactDOM.render(<div>sssss</div>,document.getElementById('app'));
